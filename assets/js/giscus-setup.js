cbfb5e48c7b85a58fc893fe90a8bd3f14d8b0cc9
KathCYM <30293568+KathCYM@users.noreply.github.com>
2026-02-15 23:17:21 +0000

    Deploying to gh-pages from @ KathCYM/kathchoi.github.io@e85787aecf7a7e8392ff0d217b3b99069ce99997 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "KathCYM/kathchoi",
+    "data-repo": "KathCYM/kathchoi.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
